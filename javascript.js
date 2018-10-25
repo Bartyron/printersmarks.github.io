87f871f6ec8b726f9a6b6327483b9b46ce0c9657
m44rtn <maartenvermeulen70@gmail.com>
2018-10-25 17:21:35 +0200

    new way to show images!!!!! yay! major version 1

---
+++ javascript.js
@@ -0,0 +1,24 @@
+
+//FIRST TIME USING JAVASCRIPT!!!
+
+var id;
+function open_close(id){
+
+    //get the element we decided to use
+    var popup = document.getElementById(id);
+
+    //show it
+    popup.classList.toggle("show");
+
+    //document.getElementById("close").style.marginTop = "150px";
+}
+
+function close(id){
+
+    //get the element we decided to use
+    var popup = document.getElementById(id);
+
+    //show it
+    popup.classList.toggle("show");
+}
+
